b563b546e8f08f55952e4a8f4e64b32458ef1ee5
smeerkahoven <smeerkahoven@gmail.com>
2019-03-17 00:37:43 -0400

    changes excel button

--- ContableEnterprise-ejb/build/generated-sources/ap-source-output/com/contabilidad/entities/NotaDebitoTransaccionPK_.java
+++ ContableEnterprise-ejb/build/generated-sources/ap-source-output/com/contabilidad/entities/NotaDebitoTransaccionPK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-15T19:30:44")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-17T00:09:13")
 @StaticMetamodel(NotaDebitoTransaccionPK.class)
 public class NotaDebitoTransaccionPK_ { 
 
--- ContableEnterprise-ejb/src/java/com/agencia/entities/BoletoSearch.java
+++ ContableEnterprise-ejb/src/java/com/agencia/entities/BoletoSearch.java
@@ -9,20 +9,7 @@ import com.seguridad.control.entities.Entidad;
 import com.seguridad.control.exception.CRUDException;
 import java.math.BigDecimal;
 import java.util.Date;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
+
 
 /**
  *
