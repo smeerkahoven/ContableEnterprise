aac7682f6940c948ef5fdefa6cb085eedb92a2b0
smeerkahoven <smeerkahoven@gmail.com>
2019-05-09 02:38:32 -0400

    add comprobante to reportes

--- ContableEnterprise-ejb/build/generated-sources/ap-source-output/com/configuracion/entities/Parametros_.java
+++ ContableEnterprise-ejb/build/generated-sources/ap-source-output/com/configuracion/entities/Parametros_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-07T23:16:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-09T01:13:36")
 @StaticMetamodel(Parametros.class)
 public class Parametros_ { 
 
--- ContableEnterprise-ejb/build/generated-sources/ap-source-output/com/seguridad/control/entities/Rol_.java
+++ ContableEnterprise-ejb/build/generated-sources/ap-source-output/com/seguridad/control/entities/Rol_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-07T23:16:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-09T01:13:36")
 @StaticMetamodel(Rol.class)
 public class Rol_ { 
 
